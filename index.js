6d52bd4f1d9da34b056368ce5c32d1a30892cb10
cjfrankcodes <cjfrankcodes@gmail.com>
2023-08-17 15:24:15 -0400

    Added emailing functionality. Improved Mobile resposniveness & layout

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 import express from "express";
 import bodyParser from "body-parser";
+import NodeMailer from 'nodemailer'
 
 const app = express();
 const port = 3000;
@@ -21,6 +22,43 @@ app.get("/gallery", (req, res) =>{
 
 app.get("/booking", (req, res) =>{
     res.render("book.ejs")
+})
+
+app.post("/submit-form", (req, res) =>{
+    const message = {
+        name : req.body.fullName,
+        email: req.body.email,
+        phone: req.body.phoneNumber,
+        date: req.body.date,
+        service: req.body.service,
+        guests: req.body.guestCount,
+        contactPref: req.body.contact
+    }
+
+    const transporter = NodeMailer.createTransport({
+        service: 'gmail',
+        auth: {
+            user: '7flavorskitchen@gmail.com',
+            pass: 'klvumbixutdarvaj'
+        }
+    })
+
+    const mailOptions = {
+        from: '7flavorskitchen@gmail.com',
+        to: '7flavorskitchen@gmail.com',
+        subject: `Booking Inquiry from ${message['name']} for a ${message['service']} on ${message['date']}`,
+        text: `${message['name']} is interested in booking a ${message['service']} on ${message['date']}. The estimated guest count is ${message['guests']}. Their phone number is ${message['phone']} and their email is ${message['email']}. They want to be contacted via ${message['contactPref']}.`
+    }
+
+    transporter.sendMail(mailOptions, (error, info) => {
+        if (error){
+            console.log(error);
+            res.render("book-error.ejs")
+        }else{
+            console.log('email sent: ' + info.response)
+            res.render("book-success.ejs")
+        }
+    })
 })
 
 app.get("/brunch", (req, res) =>{
